bc4fa69af9522d85ee1fcf498c3738e96baeb213
Alessandro Rabitti <axel83.beta@gmail.com>
2018-10-25 19:48:07 +0200

    Refactor Reader test

--- src/class/reader.spec.ts
+++ src/class/reader.spec.ts
@@ -1,20 +1,37 @@
 import { expect } from 'chai';
 import * as fs from 'fs';
+import * as util from 'util';
 import { Reader } from './reader';
 
 describe('#Reader', () => {
   let reader: Reader;
+  let done: Function;
 
   beforeEach(() => {
     reader = new Reader();
   });
 
+  describe('#constructor', () => {
+    it('should call promisify', () => {
+      expect(util).to.respondTo('promisify');
+    });
+  });
+
   describe('#readFeatureFilesFromFolder', () => {
+    beforeEach(() => {
+      done = () => {};
+    });
+
     it('should call readdir', () => {
-      const done = () => {};
       reader.readFeatureFilesFromFolder('./', done);
 
       expect(fs).to.respondTo('readdir');
+    });
+
+    it('should call stat', () => {
+      reader.readFeatureFilesFromFolder('./', done);
+
+      expect(fs).to.respondTo('stat');
     });
   });
 });
--- src/class/reader.ts
+++ src/class/reader.ts
@@ -1,50 +1,61 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as util from 'util';
 
 export class Reader {
+  private readDirAsync: Function;
+  private statAsync: Function;
   private foldersToExlcude: string[] = ['node_modules', 'dist', '.git'];
   private extensionToRead = '.feature';
+
+  constructor () {
+    this.readDirAsync = util.promisify(fs.readdir);
+    this.statAsync = util.promisify(fs.stat);
+  }
 
   readFeatureFilesFromFolder(folder: string, onReadFilesFromFolder: Function): void {
     let results: string[] = [];
 
-    fs.readdir(folder, (readdirError: Error, readFilesList: string[]) => {
-      if (readdirError) {
-        return onReadFilesFromFolder(readdirError, []);
-      }
+    this.readDirAsync(folder)
+      .then((readFilesList: string[]) => {
+        let pending = readFilesList.length;
+        if (!pending) {
+          return onReadFilesFromFolder(null, results);
+        }
 
-      let pending = readFilesList.length;
-      if (!pending) {
-        return onReadFilesFromFolder(null, results);
-      }
+        readFilesList.forEach((readFile: string) => {
+          readFile = path.resolve(folder, readFile);
 
-      readFilesList.forEach((readFile: string) => {
-        readFile = path.resolve(folder, readFile);
+          this.statAsync(readFile)
+            .then((stat: any) => {
+              if (stat && stat.isDirectory()) {
+                if (this.foldersToExlcude.includes(readFile)) {
+                  return;
+                }
 
-        fs.stat(readFile, (statError, stat) => {
-          if (stat && stat.isDirectory()) {
-            if (this.foldersToExlcude.includes(readFile)) {
-              return;
-            }
+                this.readFeatureFilesFromFolder(readFile, (readFilesFromFolderError: Error, readData: string[]) => {
+                  results = results.concat(readData);
+                  if (!--pending) {
+                    onReadFilesFromFolder(null, results);
+                  }
+                });
+              } else {
+                if (readFile.indexOf(this.extensionToRead) >= 0) {
+                  results.push(readFile);
+                }
 
-            this.readFeatureFilesFromFolder(readFile, (readFilesFromFolderError: Error, readData: string[]) => {
-              results = results.concat(readData);
-              if (!--pending) {
-                onReadFilesFromFolder(null, results);
+                if (!--pending) {
+                  onReadFilesFromFolder(null, results);
+                }
               }
+            })
+            .catch((statError: Error) => {
+              return onReadFilesFromFolder(statError, []);
             });
-          } else {
-
-            if (readFile.indexOf(this.extensionToRead) >= 0) {
-              results.push(readFile);
-            }
-
-            if (!--pending) {
-              onReadFilesFromFolder(null, results);
-            }
-          }
         });
+      })
+      .catch((readdirError: Error) => {
+        return onReadFilesFromFolder(readdirError, []);
       });
-    });
   }
 }

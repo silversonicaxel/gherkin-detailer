40bb9269282865c9bce24beca302a8627b2c48b8
Alessandro Rabitti <axel83.beta@gmail.com>
2018-11-15 23:21:56 +0100

    Implement first analysis

--- src/class/reporter.ts
+++ src/class/reporter.ts
@@ -1,11 +1,14 @@
 import { Reader } from './reader';
+import { Analyzer } from './analyzer';
 
 export class Reporter {
   private reader: Reader;
+  private analyzer: Analyzer;
   private folderToReport = './';
 
   constructor() {
     this.reader = new Reader();
+    this.analyzer = new Analyzer();
     this.reportFeaturesFiles = this.reportFeaturesFiles.bind(this);
   }
 
@@ -21,10 +24,15 @@ export class Reporter {
       return;
     }
 
+    const rowsFiles: any = [];
+
     readFiles
       .map(async (readFile: string) => {
         const contentFile = await this.reader.readContentFeatureFile(readFile);
-        this.reader.getRowsFeatureFile(contentFile);
+        const rowsFile = this.reader.getRowsFeatureFile(contentFile);
+
+        rowsFiles[readFile] = this.analyzer.getGherkins(rowsFile);
+        console.log(rowsFiles);
       });
 
     return;

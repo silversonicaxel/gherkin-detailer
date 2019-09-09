cd88a51bf056d97b4079ea6bce25fce0d977275f
Alessandro Rabitti <axel83.beta@gmail.com>
2019-09-09 22:34:51 +0200

    Upgrade libraries and bump to version 2.1.3

--- src/class/configurer.ts
+++ src/class/configurer.ts
@@ -12,7 +12,7 @@ export class Configurer {
   }
 
   private setupOptions(): void {
-    const version = '2.1.2';
+    const version = '2.1.3';
 
     program
       .version(version, '-v, --version')
